c51f0fc6823bef97a6b1e8cc54da2fce420df3c4
Frankie <francesca.beckingham@booking.com>
2024-09-20 12:18:57 +0100

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContent}>
-                <p>© 2024 Frankie's Movies. All rights reserved.</p>
+                <p>© 2024 Frankie&#39;s Movies. All rights reserved.</p>
                 <p>This website uses the TMDB API for movie data, but it is not endorsed or certified by TMDB.</p>
                 <p className={styles.easterEgg}>
                     <a href="https://www.youtube.com/shorts/kWQlwtWDJKw" target="_blank" rel="noopener noreferrer">
-                        You said no extra marks for cat videos, but here's a link to a video of my cat because this website is inspired by her!
+                        You said no extra marks for cat videos, but here&#39;s a link to a video of my cat because this website is inspired by her!
                     </a>
                 </p>
             </div>
