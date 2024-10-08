e355c33583b5c1a36ee2de9ddac81deb99dfcdbb
Frankie <francesca.beckingham@booking.com>
2024-10-08 16:00:55 +0100

    advanced search page ui improvements

--- src/app/advancedSearch/searchResultsGrid.js
+++ src/app/advancedSearch/searchResultsGrid.js
@@ -1,7 +1,14 @@
 "use client";
+import { useRouter } from 'next/navigation';
 import styles from './searchResultsGrid.module.css';
 
 const SearchResultsGrid = ({ movies }) => {
+    const router = useRouter();
+
+    const handleSeeMoreClick = (id) => {
+        router.push(`/movies/${id}`);
+    };
+
     if (!movies || movies.length === 0) {
         return <div>No movies found.</div>;
     }
@@ -10,12 +17,25 @@ const SearchResultsGrid = ({ movies }) => {
         <div className={styles.gridContainer}>
             {movies.map(movie => (
                 <div key={movie.id} className={styles.movieItem}>
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                        alt={movie.title}
-                        className={styles.moviePoster}
-                    />
-                    <h3 className={styles.movieTitle}>{movie.title}</h3>
+                    <div className={styles.moviePosterContainer}>
+                        <img
+                            src={movie.poster_path
+                                ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                                : `MoviePosterUnavailable.png`}
+                            alt={movie.title}
+                            className={styles.moviePoster}
+                        />
+                        <div
+                            className={styles.movieOverlay}
+                            onClick={() => handleSeeMoreClick(movie.id)}
+                        >
+                            <h3 className={styles.movieTitle}>{movie.title}</h3>
+                            <p className={styles.movieDescription}>
+                                {movie.overview.length > 100 ? `${movie.overview.substring(0, 100)}...` : movie.overview}
+                            </p>
+                            <span className={styles.seeMoreText}>See More</span>
+                        </div>
+                    </div>
                 </div>
             ))}
         </div>
